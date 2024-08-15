041b2d709cce346217e33f9433967a1c14f54f90
lestrangeqq <lestrangeqq@gmail.com>
2024-08-15 13:55:40 +0300

    Finished logic of the app: filter by input now works and show users

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,44 @@
+import { useState } from 'react';
 import data from '../data/users.json';
 
 export default function Main() {
+  let [users, setUsers] = useState([]);
+
+  function handleUsers(input) {
+    setUsers(data.filter((user) => user.name.includes(input)));
+  }
+
   return (
     <div>
-      <Form />
-      <Output />
+      <Form action={(e) => handleUsers(e.target.value)}/>
+      <Output users={users} data={data} />
     </div>
   );
 }
 
-function Form() {
+function Form({action}) {
   return (
     <div>
-      <input type="text"></input>
+      <input type="text" onInput={action}></input>
     </div>
   );
 }
 
-function Output() {
+function Output({users, data}) {
+  if (users.length === 0) {
   return (
     <ul>
       {data.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+    
+  }
+
+  return (
+    <ul>
+      {users.map((user) => (
         <li key={user.id}>{user.name}</li>
       ))}
     </ul>
